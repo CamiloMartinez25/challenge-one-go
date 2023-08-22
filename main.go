7e6503425102b220f0f9767dd58ad15cfcb68e98
Camilo  Martinez <camilo_2510@hotmail.com>
2023-08-21 19:53:10 -0500

    adjusting messages and number formatting

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/CamiloMartinez25/challenge-one-go/internal/tickets"
 )
@@ -22,14 +23,16 @@ const (
 	averageDestination = "ticketsAverageByDestination"
 )
 
+var wg *sync.WaitGroup
+
 func main() {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Fatal(err)
 		}
 	}()
-	//total, err := tickets.GetTotalTickets("Brazil")
 
+	wg = new(sync.WaitGroup)
 	fileInfo := getFileInfo(filename)
 
 	storage := tickets.Storage{
@@ -38,24 +41,24 @@ func main() {
 
 	//fmt.Println(storage)
 
-	canalTickets := make(chan string)
-	defer close(canalTickets)
-	canalErr := make(chan error)
-	defer close(canalErr)
+	chTickets := make(chan string)
+	chError := make(chan error)
 
-	var destination string = "Brazil"
+	var destination string = "Colombia"
 
-	go getTicketsInfo(canalTickets, canalErr, byDestination, destination, morning, storage)
+	wg.Add(3)
 
-	go getTicketsInfo(canalTickets, canalErr, byTime, destination, morning, storage)
+	go getTicketsInfo(chTickets, chError, byDestination, destination, morning, storage)
+	go getTicketsInfo(chTickets, chError, byTime, destination, earlyMorning, storage)
+	go getTicketsInfo(chTickets, chError, averageDestination, destination, afternoon, storage)
 
-	go getTicketsInfo(canalTickets, canalErr, averageDestination, destination, morning, storage)
+	go func() {
+		wg.Wait()
+		close(chTickets)
+		close(chError)
+	}()
 
-	for ticketMsj := range canalTickets {
-
-		fmt.Println(ticketMsj)
-	}
-
+	printChannelSInfo(chTickets, chError)
 }
 
 func getFileInfo(filename string) []string {
@@ -70,49 +73,59 @@ func getFileInfo(filename string) []string {
 }
 
 func getTicketsInfo(chTicket chan string, chErr chan error, infoType string, destination string, time string, storage tickets.Storage) {
-
+	defer wg.Done()
 	switch infoType {
 	case "ticketsByDestination":
 		tickets, err := storage.GetTotalTicketsByDestination(destination)
 		if err != nil {
 			chErr <- err
-			return
+		} else {
+			chTicket <- fmt.Sprintf("The amount of tickets for the destination of %s is: %v", destination, strconv.Itoa(tickets))
 		}
-
-		chTicket <- "The quantity of tickets for " + destination + " is: " + strconv.Itoa(tickets)
 
 	case "ticketsByTime":
 		tickets, err := storage.GetTotalTicketsByTime(time)
 		if err != nil {
 			chErr <- err
-			return
+		} else {
+			chTicket <- fmt.Sprintf("The amount of tickets for the %s time is: %v", time, strconv.Itoa(tickets))
 		}
-
-		chTicket <- "The quantity of tickets for the " + time + " is: " + strconv.Itoa(tickets)
 
 	case "ticketsAverageByDestination":
 		tickets, err := storage.AveragePassengersByDestination(destination)
 		if err != nil {
 			chErr <- err
-			return
+		} else {
+			chTicket <- fmt.Sprintf("The average of tickets for the destination of %s is: %.2f%%", destination, tickets)
 		}
-
-		chTicket <- "The average of tickets for " + destination + " is: " + fmt.Sprintf("%v", tickets) + "%"
 
 	default:
 		chErr <- errors.New("The info required is not available for the tickets")
 	}
 
-	printChanelsInfo(chTicket, chErr)
-
 }
 
-func printChanelsInfo(chTicket chan string, chErr chan error) {
-	select {
-	case pr := <-chTicket:
-		fmt.Println(pr)
-	case err := <-chErr:
-		log.Fatal(err)
-		os.Exit(1)
+func printChannelSInfo(chTickets chan string, chError chan error) {
+	for {
+		select {
+		case x, ok := <-chTickets:
+			if ok {
+				fmt.Println("Ticket:", x)
+			} else {
+				fmt.Println("All tickets request are closed")
+				chTickets = nil
+			}
+		case x, ok := <-chError:
+			if ok {
+				fmt.Println("Ticket Error:", x)
+			} else {
+				fmt.Println("All tickets error are closed")
+				chError = nil
+			}
+		default:
+			if chError == nil && chTickets == nil {
+				os.Exit(1)
+			}
+		}
 	}
 }
